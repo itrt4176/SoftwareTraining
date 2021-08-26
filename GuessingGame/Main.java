d6db0fdd98cc0566737de6c02797814bd995f0be
fabrice-fouron <fouronfp05@gmail.com>
2021-08-26 17:15:21 -0400

    Added a simple project

---
+++ GuessingGame/Main.java
@@ -0,0 +1,8 @@
+package GuessingGame;
+
+public class Main {
+    // Run the program here
+    public static void main(String[] args) {
+        int randomNumber = (int) (Math.random() * 100) + 1;
+    }
+}
